61775206510f5da217ad524454427817efe6bbf9
SaltySalt77 <hyna0707@gmail.com>
2024-09-29 02:10:55 +0900

    Feat: 닉네임 변경 구현 중

--- src/page/main/panel/MainTitle.jsx
+++ src/page/main/panel/MainTitle.jsx
@@ -1,10 +1,16 @@
 import { EditIcon } from '@/components/icons';
 import palette from '@/constants/palette';
+// import textStyles from '@/constants/textStyles';
 import { IconButton, Stack, Typography } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 
 const MainTitle = ({ nickname }) => {
     const isRow = nickname.length <= 5;
+    const [currNickname, setCurrNickname] = useState(nickname);
+
+    const handleChange = (e) => {
+        setCurrNickname(e.target.value);
+    };
     return (
         <Stack
             spacing={isRow ? 0.5 : 0.25}
@@ -12,14 +18,22 @@ const MainTitle = ({ nickname }) => {
             justifyContent={isRow ? 'flex-start' : 'row'}
             direction={isRow ? 'row' : 'column'}
         >
-            <Typography variant='Heading1' fontSize={'1.5rem'}>
-                <span style={{ color: palette.main2 }}>{nickname}</span>
+            <Typography variant='Heading1'>
+                <input
+                    onChange={handleChange}
+                    style={{
+                        color: palette.main2,
+                        border: 'none',
+                        outline: 'none',
+                        width: `${currNickname.length + 1}ch`,
+                        // ...textStyles.Heading1,
+                    }}
+                    value={nickname}
+                />
                 님의
             </Typography>
             <Stack direction={'row'} alignItems={'center'} spacing={'0.25rem'}>
-                <Typography variant='Heading1' fontSize={'1.5rem'}>
-                    스노우볼
-                </Typography>
+                <Typography variant='Heading1'>스노우볼</Typography>
                 <IconButton
                     sx={{
                         w: '1.5rem !important',
