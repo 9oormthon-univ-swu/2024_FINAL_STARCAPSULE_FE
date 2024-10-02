f694d4a60825979b118d34fffcd7b795c8c86634
SaltySalt77 <hyna0707@gmail.com>
2024-10-02 15:08:15 +0900

    Design: 배경 색상 적용

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ThemeProvider } from '@mui/material';
 import theme from '../src/constants/theme';
 import '../src/App.css';
+import '../src/index.css';
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
