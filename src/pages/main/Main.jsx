8ff35f24f7180c2eac3465f27d9074916c1a87fe
SaltySalt77 <hyna0707@gmail.com>
2024-10-05 18:10:36 +0900

    Feat: API 받기 위한 임시 틀 추가

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,12 +1,14 @@
-import { Button, Stack, Typography } from '@mui/material';
+import { Button, Stack, styled, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import DDayTitle from './DDayTitle';
 import MainTitle from './MainTitle';
 import Snowball from './Snowball/Snowball';
 import { ObjectNames } from '@/constants/ObjectNames';
 import Layout from '@/layouts/Layout';
+import useSWR from 'swr';
 
 // 임시 적용 데이터
+
 const memories = [
     { id: 1, writer_name: '닉네임', object_name: ObjectNames.SNOWMAN },
     { id: 2, writer_name: '닉네임', object_name: ObjectNames.SNOWMAN },
@@ -16,66 +18,69 @@ const memories = [
     { id: 6, writer_name: '닉네임', object_name: ObjectNames.SANTA_SLEIGH },
 ];
 
+const getData = async (url) => {
+    console.log(`fetching data ${url}`);
+    return {
+        capsule: {
+            id: 1,
+            snowball_name: '나의 캡슐',
+            received: 15,
+            self: 3,
+            page: 1,
+            total_page: 3,
+            memories: memories,
+        },
+    };
+};
+
+const MainContainer = styled(Stack)(() => ({
+    padding: '1rem 0 2.25rem 0',
+    boxSizing: 'border-box',
+    flexGrow: 2,
+    height: '100svh',
+}));
+
+const StyledButton = styled(Button)(({ theme }) => ({
+    boxSizing: 'border-box',
+    width: '100%',
+    height: '3.875rem',
+    backgroundColor: theme.palette.custom.button1,
+    color: theme.palette.custom.white,
+    borderRadius: '1.25rem',
+    flexGrow: 0,
+    padding: '1.25rem 0',
+    boxShadow: '0px 0px 4px 0px rgba(40, 40, 40, 0.20)',
+}));
+
 const Main = () => {
     const [nickname, setNickname] = useState('닉네임');
+    const { data } = useSWR('data', getData);
     return (
-        <Layout
-            sx={{
-                overflow: 'hidden',
-            }}
-        >
-            <Stack
+        <Layout sx={{ overflow: 'hidden' }}>
+            <MainContainer
                 direction={'column'}
                 justifyContent={'space-between'}
                 alignContent={'center'}
                 spacing={1}
-                height={'100svh'}
-                sx={{
-                    pt: '1rem',
-                    pb: '2.25rem',
-                    boxSizing: 'border-box',
-                    flexGrow: 2,
-                }}
             >
-                <Stack
-                    direction={'column'}
-                    spacing={1}
-                    sx={
-                        {
-                            // flexGrow: 2,
-                        }
-                    }
-                >
+                <Stack direction={'column'} spacing={1} sx={{ flexGrow: 2 }}>
                     <DDayTitle />
                     <MainTitle nickname={nickname} setNickname={setNickname} />
                 </Stack>
 
                 <Snowball
-                    memories={memories}
-                    current={1}
-                    total={3}
-                    received={5}
-                    self={3}
+                    memories={data.capsule.memories}
+                    current={data.capsule.page}
+                    total={data.capsule.total_page}
+                    received={data.capsule.received}
+                    self={data.capsule.self}
                     onLeftClick={() => console.log('clicked left')}
                     onRightClick={() => console.log('clicked right')}
                 />
-                <Button
-                    variant={'contained'}
-                    sx={{
-                        boxSizing: 'border-box',
-                        width: '100%',
-                        height: '3.875rem',
-                        backgroundColor: 'custom.button1',
-                        color: 'custom.white',
-                        borderRadius: '1.25rem',
-                        flexGrow: 0,
-                        py: '1.25rem',
-                        boxShadow: '0px 0px 4px 0px rgba(40, 40, 40, 0.20)',
-                    }}
-                >
+                <StyledButton variant={'contained'} sx={{}}>
                     <Typography variant='title2'>추억 전달하기</Typography>
-                </Button>
-            </Stack>
+                </StyledButton>
+            </MainContainer>
         </Layout>
     );
 };
