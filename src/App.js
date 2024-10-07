66f92fbc625bd25dd0c74c06f1baf37cd6705cef
SaltySalt77 <hyna0707@gmail.com>
2024-10-08 01:01:53 +0900

    Feat: id 없는 main 경로 추가

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/Onboarding/LoginPage';
 import PopupPage from './pages/Onboarding/PopupPage';
-import SnowballMake from './pages/Onboarding/SnowballMake'; 
+import SnowballMake from './pages/Onboarding/SnowballMake';
 import Text from './pages/text';
 import RecordForm from './pages/Record/RecordForm';
 import TextMessage from './pages/textmessage';
@@ -17,14 +17,16 @@ function App() {
         <ThemeProvider theme={theme}>
             <Router>
                 <Routes>
-                    <Route path="/" element={<LoginPage />} />
-                    <Route path="/popup" element={<PopupPage />} />
-                    <Route path="/snowballmake" element={<SnowballMake />} /> 
-                    <Route path="/text" element={<Text />} />               
-                    <Route path="/textmessage" element={<TextMessage />} /> 
-                    <Route path="/main/:id" element={<Main />} /> {/* 사용자 ID를 포함한 경로 */}
-                    <Route path="/record" element={<RecordForm />} />
-                    <Route path="/complete" element={<CreationComplete />} />
+                    <Route path='/' element={<LoginPage />} />
+                    <Route path='/popup' element={<PopupPage />} />
+                    <Route path='/snowballmake' element={<SnowballMake />} />
+                    <Route path='/text' element={<Text />} />
+                    <Route path='/textmessage' element={<TextMessage />} />
+                    <Route path='/main' element={<Main />} />{' '}
+                    {/* 사용자 ID를 포함한 경로 */}
+                    <Route path='/main/:id' element={<Main />} />{' '}
+                    <Route path='/record' element={<RecordForm />} />
+                    <Route path='/complete' element={<CreationComplete />} />
                 </Routes>
             </Router>
         </ThemeProvider>
--- src/pages/main/DDayTitle.jsx
+++ src/pages/main/DDayTitle.jsx
@@ -1,34 +1,44 @@
 import palette from '@/constants/palette';
-import { Typography } from '@mui/material';
+import { styled, Typography } from '@mui/material';
 import React from 'react';
 
+const StyledTypography = styled(Typography)(({ theme }) => ({
+    color: theme.palette.custom.grey,
+    height: '1.5rem',
+}));
+
 const DDayTitle = () => {
-    const date = new Date();
+    const today = new Date();
+    const year = today.getFullYear();
 
-    const year = date.getFullYear();
+    const startDate = new Date(year, 10, 30);
+    const endDate = new Date(year, 11, 31);
+    const dec30 = new Date(year, 11, 30);
 
-    const dDay = new Date(year, 12, 30);
+    if (today >= startDate && today <= dec30) {
+        const timeDiff = endDate - today;
+        const daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
 
-    const diff = Math.ceil(
-        (dDay.getTime() - date.getTime()) / (1000 * 60 * 60 * 24)
-    );
-
-    return (
-        <Typography
-            variant='title3'
-            sx={{ color: 'custom.grey', height: '1.5rem' }}
-        >
-            {'추억이 '}
-            <span
-                style={{
-                    color: palette.main2,
-                }}
-            >
-                {`${diff}일`}
-            </span>
-            {' 뒤에 공개돼요!'}
-        </Typography>
-    );
+        return (
+            <StyledTypography variant='title3'>
+                {'추억이 '}
+                <span
+                    style={{
+                        color: palette.main2,
+                    }}
+                >
+                    {`${daysLeft}일`}
+                </span>
+                {' 뒤에 공개돼요!'}
+            </StyledTypography>
+        );
+    } else {
+        return (
+            <StyledTypography variant='title3'>
+                {'쌓인 추억이 공개되었습니다!'}
+            </StyledTypography>
+        );
+    }
 };
 
 export default DDayTitle;
