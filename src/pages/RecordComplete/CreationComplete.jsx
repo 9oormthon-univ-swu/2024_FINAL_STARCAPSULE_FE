2b4b36d612e5a7457fd4f3adb147f2ffe7fa39aa
yeondub1121 <kssjshyjj@naver.com>
2024-10-10 03:29:54 +0900

    Feat: 내 기록/타인 기록 완료 페이지

---
+++ src/pages/RecordComplete/CreationComplete.jsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import frameSVG from '../../assets/Frame_26085556.svg'; 
+import React from 'react';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  max-width: 480px; 
+  position: relative;
+  overflow: hidden;
+  margin: 0 auto; 
+  background-color: #27405E;
+`;
+
+const SubTitle = styled.p`
+  font-size: 24px;
+  position: absolute; 
+  bottom: 550px;
+  transform: translateX(-50%);
+  left: 50%; 
+  color: white;
+  white-space: nowrap; 
+  line-height: 1.5;
+  text-align: center; 
+  font-family: "Noto Sans";
+  font-weight: 700;
+`;
+
+const SnowballName = styled.span`
+  color: #DDB892; 
+`;
+
+const SVGImage = styled.img`
+  position: absolute;
+  bottom: 380px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 250px; 
+  height: 150px;
+`;
+
+const CreationComplete = () => {
+  
+  const snowballName = localStorage.getItem('snowballName') || '이름'; 
+
+  return (
+    <Container>
+      <SubTitle>
+        <SnowballName>{snowballName}</SnowballName>님과의<br />
+        추억이 전달되었어요
+      </SubTitle>
+     
+      <SVGImage src={frameSVG} alt="Frame SVG" />
+    </Container>
+  );
+};
+
+export default CreationComplete;
