2b8150437412f43e6876839ead681c858bcfc5c2
yeondub1121 <kssjshyjj@naver.com>
2024-10-18 16:02:30 +0900

    Feat: 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,8 @@ import GuestForm from './pages/Record/GuestForm';
 import Guest from './pages/guest/Guest';
 import CalendarPage from './pages/main/CalendarPage';
 import Test from './pages/Record/Test';
-import RecordFormAfter from './pages/Record/RecordFormAfter';
+import RecordFormAfter from './pages/RecordComplete/RecordFormAfter';
+import GuestFormAfter from './pages/RecordComplete/GuestFormAfter';
 import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
 import utc from 'dayjs/plugin/utc'; // utc 플러그인
