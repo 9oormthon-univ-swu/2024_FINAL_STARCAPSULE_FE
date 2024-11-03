63d042eea6772e29e7b11825a53dbd3eca6ba2c5
JiWhyeon <jjh56992@gmail.com>
2024-11-03 16:04:48 +0900

    Fix: 이미지공유 alert만 일단 띄우기

--- src/components/ImgShareButton.jsx
+++ src/components/ImgShareButton.jsx
@@ -7,7 +7,9 @@ const ImgShareButton = ({ title, url }) => {
     const handleImageShare = async () => {
         try {
             const captureImg = document.getElementById('capture-container');
-            const canvas = await html2canvas(captureImg);
+            const canvas = await html2canvas(captureImg, {
+                backgroundColor: null,
+            });
             const dataUrl = canvas.toDataURL('image/png');
             const blob = await (await fetch(dataUrl)).blob();
             const file = new File([blob], 'layout-image.png', {
@@ -21,6 +23,7 @@ const ImgShareButton = ({ title, url }) => {
                     files: [file],
                     url: url,
                 });
+                alert('공유가 완료되었습니다!'); // 성공 알림
             } else {
                 const link = document.createElement('a');
                 link.href = dataUrl;
@@ -32,48 +35,10 @@ const ImgShareButton = ({ title, url }) => {
             }
         } catch (error) {
             console.error('공유 실패:', error);
+            alert('공유에 실패했습니다. 다시 시도해 주세요.'); // 오류 알림
         }
     };
-    // const handleImageShare = async () => {
-    //     // 캡쳐할 영역의 id 값
-    //     const captureImg = document.getElementById('capture-container');
 
-    //     // html2canvas를 사용하여 캡처
-    //     const canvas = await html2canvas(captureImg);
-    //     const dataUrl = canvas.toDataURL('image/png');
-
-    //     // 이미지 공유를 위한 링크 생성 / 클립보드에 복사
-    //     const blob = await (await fetch(dataUrl)).blob();
-    //     const file = new File([blob], 'layout-image.png', {
-    //         type: 'image/png',
-    //     });
-
-    //     if (navigator.share) {
-    //         // Web Share API를 지원하는 경우
-    //         await navigator.share({
-    //             title: title,
-    //             text: '스노우볼 이미지 공유 완료',
-    //             files: [file],
-    //             url: url,
-    //         });
-    //     } else {
-    //         // Web Share API가 지원되지 않는 경우
-    //         const link = document.createElement('img_link');
-    //         link.href = dataUrl;
-    //         link.download = 'layout-image.png';
-    //         link.click();
-
-    //         // 링크를 클립보드에 복사
-    //         navigator.clipboard
-    //             .writeText(url)
-    //             .then(() => {
-    //                 alert('링크가 클립보드에 복사되었습니다: ' + url);
-    //             })
-    //             .catch((error) => {
-    //                 console.error('링크 복사 실패:', error);
-    //             });
-    //     }
-    // };
     return (
         <IconButton
             aria-label='이미지 공유하기'
