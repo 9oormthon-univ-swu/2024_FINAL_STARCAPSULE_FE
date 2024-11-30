7432b7b93c55bf5a832266646d2bc85c4a79914e
yeondub1121 <kssjshyjj@naver.com>
2024-11-30 10:39:15 +0900

    Design: 디자인 수정

--- src/pages/RecordComplete/ImageSaveButton.jsx
+++ src/pages/RecordComplete/ImageSaveButton.jsx
@@ -25,7 +25,7 @@ const formbtn = {
     alignItems: 'center',
     borderRadius: '1.25rem',
     color: 'white',
-    background: '#7F5539', 
+    background: 'var(--main1, #405EAB)', 
     boxShadow: '0px 0px 4px 0px rgba(40, 40, 40, 0.20)',
     marginTop: '2.25rem',
     marginBottom: '1.94rem',
