d840b7cea63bfbbdfffab949231f0cb468cbb66f
SaltySalt77 <hyna0707@gmail.com>
2024-11-30 13:47:47 +0900

    Feat: pwa 자동 업데이트 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,17 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/service-worker.js')
+            .then((registration) => {
+                console.log('Service Worker registered:', registration);
+            })
+            .catch((err) => {
+                console.error('Service Worker registration failed:', err);
+            });
+    });
+}
+
 createRoot(document.getElementById('root')).render(<App />);
